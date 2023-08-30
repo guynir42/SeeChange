7fcf7d26f0fde38a69490f7ecb7243b57dde21ad
Guy Nir <guyynir@gmail.com>
2023-08-30 18:30:12 +0300

    try this

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -17,9 +18,10 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+default_config_file = str((pathlib.Path(__file__).parent.parent / 'default_config.yaml').resolve())
 from models import *
 import util.config
-util.config.Config.init(os.getenv('SEECHANGE_CONFIG', 'default_config.yaml'))
+util.config.Config.init(os.getenv('SEECHANGE_CONFIG'), default_config_file)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
