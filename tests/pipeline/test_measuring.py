6e7f35d0c4da4d16f1b01cc291bdcb0ef418d033
Guy Nir <guyynir@gmail.com>
2024-03-18 09:22:58 +0200

    fix type

--- tests/pipeline/test_measuring.py
+++ tests/pipeline/test_measuring.py
@@ -5,9 +5,10 @@ import numpy as np
 
 def test_measuring(measurer, decam_cutouts):
     measurer.pars.test_parameter = uuid.uuid4().hex
-    decam_cutouts[0] =
+    sz = decam_cutouts[0].sub_data.shape
+    decam_cutouts[0].sub_data = np.zeros_like(decam_cutouts[0].sub_data)
+    decam_cutouts[0].sub_data[sz[0]//2, sz[1]//2] = 1
     ds = measurer.run(decam_cutouts)
-
 
     assert len(ds.measurements) == len(ds.cutouts)
 
